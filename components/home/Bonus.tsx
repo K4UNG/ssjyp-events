e7d9982e3f5ddaa58ba175e3ab49fedfe0b5d565
K4UNG <kgzinhein.my@gmail.com>
2022-09-29 00:46:15 +0630

    bonus section responsive

--- components/home/Bonus.tsx
+++ components/home/Bonus.tsx
@@ -44,8 +44,8 @@ const data = [
 
 function Bonus() {
   return (
-    <div className="mt-8">
-      <h2 className="font-caudex font-bold text-2xl mb-4">
+    <div className="mt-8 lg:mt-12 lg:mb-8">
+      <h2 className="font-caudex font-bold text-2xl mb-4 lg:text-2.5xl">
         Active Bonus Rewards
       </h2>
       <select className="border border-black px-2 mb-4">
@@ -56,7 +56,7 @@ function Bonus() {
       {data.map((item) => {
         return (
           <div key={item.name} className="mb-8">
-            <h3 className="flex font-bold text-2xl items-center mb-2">
+            <h3 className="flex font-bold text-2xl items-center mb-2 uppercase">
               {item.name}
               <div className="relative w-8 h-6 ml-1">
                 <Image
@@ -67,7 +67,7 @@ function Bonus() {
                 />
               </div>
             </h3>
-            <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 lg:grid-cols-5">
               {item.albums.map((album) => {
                 return (
                   <AlbumItem
