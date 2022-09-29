eb755ce1f082934b03e0b8faf96c1f003deb50e8
K4UNG <kgzinhein.my@gmail.com>
2022-09-29 22:40:12 +0630

    add artist page

---
+++ components/artist/Artist.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Artist } from "../../pages/artist/[slug]";
+import Image from "next/image";
+import { urlFor } from "../../sanity";
+import Link from "next/link";
+import BackBtn from "../ui/BackBtn";
+
+function Artist({ data }: { data: Artist }) {
+  return (
+    <div className="sm:grid content-center">
+      <BackBtn />
+      <div className="sm:grid sm:grid-cols-[1fr_1.3fr] sm:items-center sm:space-x-8">
+        <div className="relative aspect-[4/5] border-[2px] border-black">
+          <Image
+            src={urlFor(data.image).url()}
+            layout="fill"
+            objectFit="cover"
+            alt={data.name}
+          />
+        </div>
+        <div className="mt-4 mb-8 space-y-2 md:space-y-6">
+          <h1 className="text-3xl md:text-4xl">
+            <span className="font-bold uppercase">{data.name}</span> (
+            {data.koreanName})
+          </h1>
+          <div className="md:text-lg">
+            <div>
+              <span className="font-bold text-lg md:text-xl">Birthdate: </span>
+              {data.birthdate}
+            </div>
+            <div>
+              <span className="font-bold text-lg md:text-xl">
+                Nationality:{" "}
+              </span>
+              {data.nationality}
+            </div>
+            <p>
+              <span className="font-bold text-lg md:text-xl">Fun Fact: </span>
+              <br />
+              {data.funfact}
+            </p>
+            <div className="font-bold text-lg md:text-xl">Members:</div>
+            <div className="flex flex-wrap">
+              {data.group.members.map((m) => {
+                if (m.name === data.name || m.group.name !== data.group.name)
+                  return;
+                return (
+                  <span key={m.slug.current}>
+                    <Link href={"/artist/" + m.slug.current}>
+                      <a className="mr-4 uppercase italic hover:font-bold font-dm text-base">
+                        {m.name}
+                      </a>
+                    </Link>
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Artist;
---
+++ pages/artist/[slug].tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import client from "../../sanity";
+import Artist from "../../components/artist/Artist";
+
+export interface Artist {
+  name: string;
+  koreanName: string;
+  birthdate: string;
+  funfact: string;
+  image: object;
+  nationality: string;
+  group: {
+    name: string;
+    members: {
+      name: string;
+      slug: {
+        current: string;
+      };
+      group: {
+        name: string;
+      };
+    }[];
+  };
+}
+
+const ArtistPage: NextPage<{ data: Artist }> = ({ data }) => {
+  return (
+    <div className="container grid mx-auto p-4 min-h-[80vh] md:py-12 lg:max-w-[960px]">
+      <Artist data={data} />
+    </div>
+  );
+};
+
+export default ArtistPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data: { slug: { current: string } }[] = await client.fetch(
+    "*[_type=='artist']{slug}"
+  );
+
+  return {
+    fallback: false,
+    paths: data.map((d) => ({ params: { slug: d.slug.current } })),
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const data: Artist[] | null = await client.fetch(
+    `*[_type=='artist' && slug.current=='${context.params?.slug}']{ name, koreanName, nationality, birthdate, funfact, image, group->{name, 'members': *[_type=='artist'] | order(birthdate) {name, slug, group->{name}}} }`
+  );
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data: data[0],
+    },
+  };
+};
