10ae5cecd39e2f01da42cf4bcac24b33594a836e
K4UNG <kgzinhein.my@gmail.com>
2022-09-30 15:38:41 +0630

    add favicon in Head tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <div>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <AnimatePresence mode="wait">
         <motion.div
