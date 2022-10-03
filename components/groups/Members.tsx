f5664854af760d3452046300d29313f8a6436c78
K4UNG <kgzinhein.my@gmail.com>
2022-10-04 01:00:04 +0630

    artist avatar text align center

--- components/groups/Members.tsx
+++ components/groups/Members.tsx
@@ -23,7 +23,7 @@ function Members({ members }: Props) {
               passHref={true}
             >
               <a className="relative w-full aspect-square rounded-full overflow-hidden outline outline-[1px] outline-black group">
-                <div className="uppercase text-white font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 text-xs text-shadow">
+                <div className="uppercase text-white font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 text-xs text-shadow text-center">
                   {member.name}
                 </div>
                 <div className="absolute w-full h-full top-0 left-0 z-10 backdrop-blur-[2px] opacity-0 group-hover:opacity-100 group-focus-visible:opacity-100 duration-150 transition-opacity" />
